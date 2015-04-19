7004f851313c7ec66b90de05f25c767cc94d2a8d
clement <maboiteaspam@wanadoo.fr>
2015-04-20 02:01:37 +0800

    first commit

--- index.js
+++ index.js
@@ -1,48 +1,191 @@
 
 var pkg = require('./package.json');
 var SSH2pool = require('ssh2-pool');
+var _ = require('underscore');
+var async = require('async');
+var path = require('path');
+var glob = require('glob');
+var fs = require('fs-extra');
 var log = require('npmlog');
 
-var NodeOps = function(servers){
+log.level = process.env['NPM_LOG'] || 'info';
+
+var NodeOps = function(servers, profiles){
+  this.profiles = profiles;
+  this.servers = servers;
   this.pool = new SSH2pool(servers);
   this.log = log;
 };
 
-NodeOps.prototype.push = function( env, localPath, remotePath, options ){
+/**
+ * Splits profile_env string
+ * format is profile:env
+ *
+ * if env is null, such profile:
+ * it will apply on all machine
+ * it will use given profile
+ *
+ * if profile is null, such :env
+ * it will apply on all machine of env
+ * it will use first profile found
+ *
+ * @param profile_env
+ * @returns {{profile: (*|null), env: string}}
+ */
+NodeOps.prototype.split_profile_env = function( profile_env ){
 
-  var tmpRemotePath = options.tmp || '/tmp';
-  var pool = this.pool;
+  profile_env = (''+profile_env);
 
-  pool.env(env).putDir(localPath, tmpRemotePath, function(){
+  var profile = '';
+  var env = '';
+
+  if(profile_env.match(/^([^:]+):(.+)$/)){
+
+    profile_env = profile_env.match(/^([^:]+):(.+)$/);
+
+    profile = profile_env[1] || null;
+    if( profile === null ){
+      profile = Object.keys(this.profiles)[0];
+    }
+
+    env = (profile_env[2]) || ':all';
+
+  } else if(profile_env.match(/.+/) ){
+    profile = Object.keys(this.profiles)[0];
+
+    env = profile_env;
+  }
+
+  if( !(profile in this.profiles) )
+    throw 'profile '+profile+' does not exist.';
+
+  if(!Object.keys(this.profiles).length)
+    throw 'profile is required.';
+
+  if(!(env in this.servers) && env !== ':all')
+    throw 'env '+env+' does not exist.';
+
+  return {
+    profile: profile,
+    env: env
+  }
+};
+
+/**
+ * Put a local directory to a remote path
+ *
+ * @param profile_env
+ * @param localDirectoryPath
+ * @param remotePath
+ * @param options
+ * @param done
+ */
+NodeOps.prototype.push = function( profile_env, localDirectoryPath, remotePath, options, done ){
+
+  var env = this.split_profile_env(profile_env).env;
+
+  var tmpRemotePath = options.tmp || '/tmp/'+(new Date()).getTime()+'';
+
+
+  var pool = this.pool.env(env);
+
+  var serverDone = 0;
+  pool.putDir(localDirectoryPath, tmpRemotePath, function(){
 
     var cmds = function(){
       return [
         'sudo rm -fr '+remotePath+'',
         'sudo mkdir -p '+remotePath+'',
-        'sudo cp -R '+tmpRemotePath+'* '+remotePath+'',
+        'sudo cp -R '+tmpRemotePath+'/* '+remotePath+'',
+        'sudo ls -alh '+remotePath+'/*',
         'msg:`All done!`'
       ]
     };
-    pool.env(env).runMultiple(cmds, function(){
-      log.info(pkg.name, 'done');
+    pool.exec(cmds, function(err,stdout){
+      serverDone++;
+      if(pool.length===serverDone){
+        log.info(pkg.name, 'done');
+        setTimeout(function(){
+          if(done) done(err,stdout);
+        },500);
+      }
     });
   });
 
 };
 
-NodeOps.prototype.run = function( env, cmd ){
-
+/**
+ * Run a command on a remote and return their outputs.
+ *
+ * @param profile_env
+ * @param cmd
+ * @param then
+ */
+NodeOps.prototype.exec = function( profile_env, cmd, then ){
+  var env = this.split_profile_env(profile_env).env;
   var pool = this.pool;
-
-  pool.env(env).sshRun([cmd], function(){
+  pool.env(env).exec([cmd], function(allSessionErr, allSessionText){
     log.info('run', 'done');
+    if(then) then(allSessionErr, allSessionText);
   });
-
 };
 
-NodeOps.prototype.tail = function(env, filePath, options){
-
+/**
+ * Run a command on a remote and return their outputs.
+ *
+ * @param profile_env
+ * @param cmd
+ * @param then
+ */
+NodeOps.prototype.exec = function( profile_env, cmd, then ){
+  var env = this.split_profile_env(profile_env).env;
   var pool = this.pool;
+  pool.env(env).exec([cmd], function(allSessionErr, allSessionText){
+    log.info('run', 'done');
+    if(then) then(allSessionErr, allSessionText);
+  });
+};
+
+/**
+ * Run a command on a remote and return a close handler.
+ * Ctrl+c would stop the process properly.
+ *
+ * @param profile_env
+ * @param cmd
+ * @param hostReady
+ * @returns {Function}
+ */
+NodeOps.prototype.run = function( profile_env, cmd, hostReady ){
+  var env = this.split_profile_env(profile_env).env;
+  var pool = this.pool;
+  var serie = [];
+  pool.env(env).run(cmd, function(success, rstdout, rstderr, server, conn){
+    serie.push(function(done){
+      conn.end();
+      done();
+    });
+    if(hostReady) hostReady(success, rstdout, rstderr, server, conn);
+  });
+  return function(done){
+    async.parallelLimit(serie,4,done);
+  };
+};
+
+/**
+ *
+ * @param profile_env
+ * @param filePath
+ * @param options
+ * @param hostReady
+ * @returns {*}
+ */
+NodeOps.prototype.tail = function(profile_env, filePath, options, hostReady){
+  var env = this.split_profile_env(profile_env).env;
+
+  if(_.isFunction(options)){
+    hostReady = options;
+    options = {};
+  }
 
   var dowhat = (options.do || 'tail');
 
@@ -53,16 +196,75 @@ NodeOps.prototype.tail = function(env, filePath, options){
     cmd = 'sudo tail -f '+filePath;
   }
 
-  pool.env(env).run(cmd,function(success, stream, stderr, server){
-    stream.removeAllListeners('data');
+  return this.run(env, cmd, function(success, stream, stderr, server, conn){
     stream.on('data', function(data){
       data.toString().split('\n').forEach(function(line){
         if(line){
-          console.log(server.name+' : '+line);
+          console.log('%s@%s > %s',
+            server.username, (server.name||server.host), line);
+        }
+      });
+    });
+    if(hostReady) hostReady(success, stream, stderr, server, conn);
+  });
+};
+
+/**
+ * Generate template files
+ * and record their output
+ * to a local directory.
+ *
+ * @param profile_env
+ * @param sourcePath
+ * @param savePath
+ * @param done
+ */
+NodeOps.prototype.generate = function( profile_env, sourcePath, savePath, done ){
+
+  profile_env = this.split_profile_env(profile_env);
+  var env = profile_env.env;
+  var profile = profile_env.profile;
+
+  log.verbose(pkg.name, 'generate %s %s', sourcePath, savePath);
+
+  var data = {
+    servers:this.pool.env(env).toJson(),
+    profile:this.profiles[profile],
+    profileName:profile,
+    // additional helper
+    path:path
+  };
+  var options = {
+    cwd: sourcePath
+  };
+  var fileDone = 0;
+  options.nodir = true;
+  glob( '**', options, function (er, files) {
+
+    files.forEach(function(f){
+      var from = path.join(sourcePath, f);
+      var to = path.join(savePath, f);
+      log.verbose(pkg.name, 'put %s %s', path.relative(sourcePath,from), path.relative(savePath,to));
+
+      fs.readFile(from, function(err, template){
+        try{
+          var compiled = _.template(''+template);
+          var result = compiled(data);
+          fs.mkdirsSync(path.dirname(to));
+          fs.writeFileSync(to,result,'utf-8');
+        }catch(ex){
+          log.error(pkg.name,ex);
+        }
+        fileDone++;
+        if(files.length == fileDone ){
+          setTimeout(function(){
+            done();
+          },500);
         }
       });
 
     });
+
   });
 };
 
---
+++ test/index.js
@@ -0,0 +1,116 @@
+
+process.env['NPM_LOG'] = process.env['NPM_LOG']
+|| 'info'
+|| 'verbose'
+|| 'silly'
+;
+
+require('should');
+var fs = require('fs-extra');
+var log = require('npmlog');
+var Vagrant = require('node-vagrant-bin');
+
+var NodeOps = require('../index.js');
+
+var servers = require('./vagrant.json');
+var profiles = require('./profiles.json');
+var op = new NodeOps(servers, profiles);
+
+var vagrant = new Vagrant();
+var hasBooted = true;
+before(function(done){
+  this.timeout(50000);
+  vagrant.isRunning(function(running){
+    if(running===false){
+      vagrant.up('precise64',function(err,booted){
+        hasBooted = booted;
+        done();
+      });
+    }else{
+      console.log('running machine '+running);
+      hasBooted = false;
+      done();
+    }
+  });
+});
+after(function(done){
+    this.timeout(50000);
+    vagrant.isRunning(function(running){
+      console.log('running machine '+running);
+      if(hasBooted){
+        vagrant.halt(function(){
+          console.log('halted');
+          done();
+        });
+      } else {
+        done();
+      }
+    });
+  });
+
+before(function(){
+  fs.removeSync(__dirname+'/app/conf_out/');
+  fs.mkdirsSync(__dirname+'/app/conf_out/');
+});
+
+describe('generate', function(){
+  this.timeout(50000)
+  it('configuration files', function(done){
+    op.generate( 'precise64::pool1', __dirname+'/app/conf_tpl/', __dirname+'/app/conf_out/', function(){
+      fs.existsSync(__dirname+'/app/conf_out/httpd/ports.conf')
+        .should.be.true;
+      fs.readFileSync(__dirname+'/app/conf_out/httpd/vhosts/www.conf','utf-8')
+        .indexOf(profiles.precise64.http.LogPath).should.not.eql(-1);
+      done();
+    });
+  });
+});
+
+describe('ssh', function(){
+  this.timeout(50000)
+  it('say hello', function(done){
+    op.exec( ':pool1', 'echo hello', function(err,stdout){
+      stdout.should.match(/hello/)
+      done();
+    });
+  });
+});
+
+
+describe('sftp', function(){
+  this.timeout(50000);
+  var localDirectoryPath = __dirname+'/app/conf_out/httpd';
+  var remotePath = profiles.precise64.http.EtcPath+'/custom';
+  before(function(done){
+    op.exec( ':pool1', 'sudo rm -fr '+remotePath+' && ls -al '+remotePath, function(){
+      done();
+    });
+  });
+  it('push directory on a remote', function(done){
+    var options = {};
+    op.push( ':pool1', localDirectoryPath, remotePath, options, function(err,stdout){
+      (!!err).should.be.false;
+      stdout.should.match(/custom\/ports\.conf/);
+      done();
+    });
+  });
+});
+
+
+describe('stream', function(){
+  this.timeout(50000);
+  it('streams file on a remote', function(done){
+    var end = op.tail( ':pool1', '/var/log/auth.log', function(err,rstdout){
+      (!!err).should.be.false;
+      var data = '';
+      rstdout.on('data',function(d){
+        data += ''+d;
+      });
+      setTimeout(function(){
+        (data).indexOf("vagrant-ubuntu-precise-64").should.not.eql(-1);
+        end(done);
+      },500);
+    });
+  });
+});
+
